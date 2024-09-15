d3a0148aadc1edf0f7ea29f50461b5485a212757
Jieun Kim <endoxff@gmail.com>
2024-09-15 23:15:14 +0900

    setting(#1): 개발 환경 설정

---
+++ src/test/java/com/shiftm/shiftm/ShiftMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftm.shiftm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
